a763648c18a13e923a0e4819b29ac086a5cf32ef
Guillem Tió Surós <gtios@hotmail.com>
2022-06-04 12:55:56 +0200

    barcomponent

---
+++ solved/SFML_TCengine/Source/Game/BarComponent.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using TCEngine;
+
+namespace TCGame
+{
+    
+    class BarComponent : BaseComponent
+    {
+        private const int MAX_PERCENTAGE = 100;
+        private int m_BarPercentage;
+        BarType m_Type;
+
+        public BarComponent(BarType type)
+        {
+            m_Type = type;
+            m_BarPercentage = MAX_PERCENTAGE;
+        }
+
+        public override EComponentUpdateCategory GetUpdateCategory()
+        {
+            return EComponentUpdateCategory.Update;
+        }
+
+        public int GetBarPercentage()
+        {
+            return m_BarPercentage;
+        }
+
+        public void IncreasePercentage(int _amount)
+        {
+            m_BarPercentage = Math.Min(m_BarPercentage + _amount, MAX_PERCENTAGE);
+        }
+
+        public void DecreasePercentage(int _amount)
+        {
+            m_BarPercentage = Math.Max(m_BarPercentage - _amount, 0);
+        }
+
+        public override object Clone()
+        {
+            BarComponent clonedComponent = new BarComponent(m_Type);
+            return clonedComponent;
+        }
+    }
+
+    public enum BarType
+    {
+        Salud,
+        Felicidad,
+        Sociabilidad,
+        Adiccion,
+    }
+}
