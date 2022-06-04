6c0ff4a05640a8eaa9010bf2097831e1b29b3366
Àlex Salueña Pujolràs <alex.saluena10@gmail.com>
2022-06-04 13:09:59 +0200

    Arreglo del SFML

--- solved/SFML_TCengine/Source/Game/SuperAwesomeGame.cs
+++ solved/SFML_TCengine/Source/Game/SuperAwesomeGame.cs
@@ -31,5 +31,105 @@ namespace TCGame
 
         }
 
+        private void CreateSaludBar()
+        {
+            Actor actor = new Actor("Horizontal Bar HUD");
+
+            TransformComponent transformComponent = actor.AddComponent<TransformComponent>();
+            transformComponent.Transform.Position = new Vector2f(600.0f, 50.0f);
+
+            BarHUDComponent barHUDComponent = actor.AddComponent<BarHUDComponent>(new Vector2f(300.0f, 40.0f));
+
+            float barDirection = 1.0f;
+
+            TimerComponent timerComponent = actor.AddComponent<TimerComponent>(0.05f);
+            timerComponent.Loop = true;
+            timerComponent.OnTime = () =>
+            {
+                barHUDComponent.FillRatio += 0.05f * barDirection;
+                if (barHUDComponent.FillRatio >= 1.0f || barHUDComponent.FillRatio <= 0.0f)
+                {
+                    barDirection = barDirection * -1.0f;
+                }
+            };
+
+            TecnoCampusEngine.Get.Scene.CreateActor(actor);
+        }
+
+        private void CreateFelicidadBar()
+        {
+            Actor actor = new Actor("Barra Felicidad");
+
+            TransformComponent transformComponent = actor.AddComponent<TransformComponent>();
+            transformComponent.Transform.Position = new Vector2f(TecnoCampusEngine.Get.Scene., 50.0f);
+
+            BarHUDComponent barHUDComponent = actor.AddComponent<BarHUDComponent>(new Vector2f(300.0f, 40.0f));
+
+            float barDirection = 1.0f;
+
+            TimerComponent timerComponent = actor.AddComponent<TimerComponent>(0.05f);
+            timerComponent.Loop = true;
+            timerComponent.OnTime = () =>
+            {
+                barHUDComponent.FillRatio += 0.05f * barDirection;
+                if (barHUDComponent.FillRatio >= 1.0f || barHUDComponent.FillRatio <= 0.0f)
+                {
+                    barDirection = barDirection * -1.0f;
+                }
+            };
+
+            TecnoCampusEngine.Get.Scene.CreateActor(actor);
+        }
+
+        private void CreateSociabilidadBar()
+        {
+            Actor actor = new Actor("Horizontal Bar HUD");
+
+            TransformComponent transformComponent = actor.AddComponent<TransformComponent>();
+            transformComponent.Transform.Position = new Vector2f(600.0f, 50.0f);
+
+            BarHUDComponent barHUDComponent = actor.AddComponent<BarHUDComponent>(new Vector2f(300.0f, 40.0f));
+
+            float barDirection = 1.0f;
+
+            TimerComponent timerComponent = actor.AddComponent<TimerComponent>(0.05f);
+            timerComponent.Loop = true;
+            timerComponent.OnTime = () =>
+            {
+                barHUDComponent.FillRatio += 0.05f * barDirection;
+                if (barHUDComponent.FillRatio >= 1.0f || barHUDComponent.FillRatio <= 0.0f)
+                {
+                    barDirection = barDirection * -1.0f;
+                }
+            };
+
+            private void CreateAdiccionBar()
+            {
+                Actor actor = new Actor("Horizontal Bar HUD");
+
+                TransformComponent transformComponent = actor.AddComponent<TransformComponent>();
+                transformComponent.Transform.Position = new Vector2f(600.0f, 50.0f);
+
+                BarHUDComponent barHUDComponent = actor.AddComponent<BarHUDComponent>(new Vector2f(300.0f, 40.0f));
+
+                float barDirection = 1.0f;
+
+                TimerComponent timerComponent = actor.AddComponent<TimerComponent>(0.05f);
+                timerComponent.Loop = true;
+                timerComponent.OnTime = () =>
+                {
+                    barHUDComponent.FillRatio += 0.05f * barDirection;
+                    if (barHUDComponent.FillRatio >= 1.0f || barHUDComponent.FillRatio <= 0.0f)
+                    {
+                        barDirection = barDirection * -1.0f;
+                    }
+                };
+
+                TecnoCampusEngine.Get.Scene.CreateActor(actor);
+            }
+
+            TecnoCampusEngine.Get.Scene.CreateActor(actor);
+        }
+
     }
 }
